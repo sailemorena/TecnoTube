7bb9f02765f152d7f017d8fd6b3c66543be56b1d
GuillermOsuna <gosuna79@gmail.com>
2015-09-12 21:30:00 -0600

    agregue todas las clases

---
+++ Tecnotube/menu.js
@@ -0,0 +1,67 @@
+$(document).ready(main)
+
+var=contador=1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		//$('nav').toggle();
+		if(contador == 1){
+			$('nav').animate({
+				left:'50'
+			});
+			contador=0;
+		} else {
+			contador=1;
+			$('nav').animate({
+				left:'10%'
+			});
+		}
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
